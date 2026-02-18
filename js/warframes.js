0bdf16ea3ec184047fd07fb17e2027950c689cda
Wf-sketch <decolorgraf@gmail.com>
2026-02-18 08:09:37 -0300

    Create warframes.js

---
+++ js/warframes.js
@@ -0,0 +1,18 @@
+const warframes = [
+    {
+        name: "Excalibur",
+        image: "https://static.wikia.nocookie.net/warframe/images/8/80/ExcaliburNewLook.png",
+        augments: [
+            {
+                name: "Purging Slash",
+                description: "Inimigos atingidos por Slash Dash têm seus buffs removidos.",
+                sound: "https://static.wikia.nocookie.net/warframe/images/7/7f/SlashDash.ogg"
+            },
+            {
+                name: "Furious Javelin",
+                description: "Aumenta dano e concede bônus por inimigo atingido.",
+                sound: "https://static.wikia.nocookie.net/warframe/images/4/4a/RadialJavelin.ogg"
+            }
+        ]
+    }
+];
